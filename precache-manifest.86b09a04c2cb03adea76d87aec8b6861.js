2ffa1fcafdb9c4fbd00f3a944ac71f1e4fc63b88
bipin swarnkar <bipinswarnkar1989@gmail.com>
2020-09-30 12:06:19 +0530

    Updates

---
+++ precache-manifest.86b09a04c2cb03adea76d87aec8b6861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3837584a5e96eeefb4217f40339dbae",
+    "url": "/shopping-cart-ui-task/index.html"
+  },
+  {
+    "revision": "5c25df6ba2733e88994b",
+    "url": "/shopping-cart-ui-task/static/css/main.c3b4dd52.chunk.css"
+  },
+  {
+    "revision": "4b63038ed710c82149f1",
+    "url": "/shopping-cart-ui-task/static/js/2.424e40ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart-ui-task/static/js/2.424e40ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c25df6ba2733e88994b",
+    "url": "/shopping-cart-ui-task/static/js/main.7e6ed5f7.chunk.js"
+  },
+  {
+    "revision": "265a80fe58e47147150d",
+    "url": "/shopping-cart-ui-task/static/js/runtime-main.a62bbaa1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/shopping-cart-ui-task/precache-manifest.86b09a04c2cb03adea76d87aec8b6861.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shopping-cart-ui-task/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
